79544347265843998647482e16782c441c634722
JoabKaimon <joab.kaimon32@gmail.com>
2024-02-06 20:39:18 -0300

    error JS

---
+++ index.js
@@ -0,0 +1,9 @@
+var Leonardo = window.document.getElementById("Leonardo")
+var Bruna = window.document.getElementById("Bruna")
+var Samanta = window.document.getElementById("Samanta")
+
+function RolarParaDireita(){
+        Leonardo.style ="display:none"
+        Bruna.style ="display:flex"
+
+}
